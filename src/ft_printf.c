f0e929ec6b38303f70b86eb3e3b1b4108b4881a2
namig41 <gusinov.namig@mail.ru>
2020-02-01 17:23:06 +0300

    print1.1

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -12,13 +12,29 @@
 
 #include "ft_printf.h"
 
-static void processing_width(char c, int w)
+static void m_putstr(char *s, char c, int w)
 {
-    int i;
+	size_t i;
+	size_t len;
 
-    i = -1;
-    while (++i < w)
-        ft_putchar(c);
+	i = 0;
+	if (s)
+	{
+		len = ft_min(ft_strlen(s), w);
+		while (i < len)
+		{
+			ft_putchar(s[i]);
+			i++;
+		}
+	}
+	else
+	{
+		while (i < w)
+		{
+			ft_putchar(c);
+			i++;
+		}
+	}
 }
 
 static void type_oct(char c)
@@ -26,18 +42,16 @@ static void type_oct(char c)
     if (g_flags.oct)
     {
         g_flags.oct = 0;
-        if (c == 'x')
-            g_flags.oct = 2;
-        else if (c == 'X')
+        if (c == 'x' || c == 'X')
             g_flags.oct = 2;
         else if (c == 'o')
             g_flags.oct = 1;
     }
 }
 
-static void print_int(char c, char *s_int)
+static void print_oct(char c, char s)
 {
-    if (g_flags.oct && s_int[0] != '0')
+    if (g_flags.oct && g_flags.zero && s != '0')
     {
         if (c == 'x')
             ft_putstr("0x");
@@ -46,103 +60,115 @@ static void print_int(char c, char *s_int)
         else if (c == 'o')
             ft_putchar('0');
     }
-    if (s_int[0] != '-' && g_flags.plus && !g_flags.minus)
+}
+
+static void print_int(char *s_int)
+{
+    if (s_int[0] != '-' && g_flags.plus && g_flags.zero)
         ft_putchar('+');
     ft_putstr(s_int);
 }
 
-static int proccesing_sign(char c)
+static void processing_sign(char c)
 {
-    if (c == '-')
-        ft_putchar('-');
-    else if (g_flags.plus)
-        ft_putchar('+');
-    else
-        return (0);
-    return (1);
+	if (g_flags.zero)
+	{
+		if (c == '-')
+			ft_putchar('-');
+		else if (g_flags.plus)
+			ft_putchar('+');
+	}
 }
 
-static void proccesing_int_flag(char c, char *s_int)
+// l
+// ll
+// h
+// hh
+static void processing_int_flag2(char c, char *s_int)
 {
-    type_oct(c);
-    if (g_flags.minus)
-    {
-        print_int(c, s_int);
-        processing_width(' ', g_flags.width - ft_strlen(s_int) - g_flags.oct);
-    }
-    else
-    {
-        g_flags.minus = g_flags.zero ? proccesing_sign(s_int[0]) : 0;
-        processing_width(g_flags.zero ? '0' : ' ', g_flags.width - ft_strlen(s_int) - g_flags.minus - g_flags.oct);
-        print_int(c, s_int + (g_flags.minus && s_int[0] == '-'));
-    }
+
 }
 
-static int  proccesing_int(char c, va_list arg)
+//static void processing_int_flag(char c, char *s_int)
+//{
+//    type_oct(c);
+//    g_flags.l = g_flags.zero;
+//    if (g_flags.point)
+//    {
+//        processing_width(' ', g_flags.width - ft_max(g_flags.point, ft_strlen(s_int)) - g_flags.oct);
+//        g_flags.minus = processing_sign(s_int[0]);
+//        g_flags.width = (g_flags.width > g_flags.point) ? g_flags.width - ft_strlen(s_int) : g_flags.point;
+//        processing_width('0', g_flags.width - ft_strlen(s_int) - g_flags.minus - g_flags.oct);
+//        print_int(c, s_int + (g_flags.minus && s_int[0] == '-'));
+//    }
+//    else if (g_flags.minus)
+//    {
+//        print_oct(c, s_int[0]);
+//        print_int(c, s_int);
+//        processing_width(' ', g_flags.width - ft_strlen(s_int) - g_flags.oct);
+//    }
+//    else
+//    {
+//        g_flags.minus = g_flags.zero ? processing_sign(s_int[0]) : 0;
+//        print_oct(c, s_int[0]);
+//        processing_width(g_flags.zero ? '0' : ' ', g_flags.width - ft_strlen(s_int) - g_flags.minus - g_flags.oct);
+//        print_int(c, s_int + (g_flags.minus && s_int[0] == '-'));
+//    }
+//}
+
+
+
+static void  processing_int(char c, va_list arg)
 {
     char *s_int;
 
     if (c == 'd' || c == 'i')
     {
-        if (g_flags.ll)
+        if (g_modifiers.ll)
             s_int = ft_lltoa(va_arg(arg, t_ll));
-        else if (g_flags.l)
+        else if (g_modifiers.l)
             s_int = ft_lltoa(va_arg(arg, t_l));
         else
             s_int = ft_lltoa(va_arg(arg, int));
     }
     else if (c == 'u')
     {
-        if (g_flags.ll)
+        if (g_modifiers.ll)
             s_int = ft_lltoa(va_arg(arg, t_ull));
-        else if (g_flags.l)
+        else if (g_modifiers.l)
             s_int = ft_lltoa(va_arg(arg, t_ul));
         else
             s_int = ft_lltoa(va_arg(arg, t_ui));
     }
     else if (c == 'o')
     { 
-        if (g_flags.ll)
+        if (g_modifiers.ll)
             s_int = ft_ulltoa_base(va_arg(arg, t_ull), 8, 'o');
-        else if (g_flags.l)
+        else if (g_modifiers.l)
             s_int = ft_ulltoa_base(va_arg(arg, t_ul), 8, 'o');
         else
             s_int = ft_uitoa_base(va_arg(arg, int), 8, 'o');
     }
     else if (c == 'x')
     {
-        if (g_flags.ll)
+        if (g_modifiers.ll)
             s_int = ft_ulltoa_base(va_arg(arg, t_ull), 16, 'x');
-        else if (g_flags.l)
+        else if (g_modifiers.l)ё
             s_int = ft_ulltoa_base(va_arg(arg, t_ul), 16, 'x');
         else
             s_int = ft_uitoa_base(va_arg(arg, int), 16, 'x');
     }
     else if (c == 'X')
     {
-        if (g_flags.ll)
+        if (g_modifiers.ll)
             s_int = ft_ulltoa_base(va_arg(arg, t_ull), 16, 'X');
-        else if (g_flags.l)
+        else if (g_modifiers.l)
             s_int = ft_ulltoa_base(va_arg(arg, t_ul), 16, 'X');
         else
             s_int = ft_uitoa_base(va_arg(arg, int), 16, 'X');
     }
-    else
-        return (0);
-    proccesing_int_flag(c, s_int);
+    processing_int_flag(c, s_int);
     free(s_int);
-    return (1);
-}
-
-static int  belongs_set(char c, char *s)
-{
-    int     len;
-
-    len = ft_strlen(s);
-    while (--len >= 0)
-        if (c == s[len])
-            return (1);
-    return (0);
 }
 
 static void  print_float(double real)
@@ -150,45 +176,31 @@ static void  print_float(double real)
     int byte;
 
     byte = -1;
-    if (!g_flags.accuracy)
-        g_flags.accuracy = 6;
+    if (!g_flags.point)
+        g_flags.point = 6;
     ft_putnbr((t_ui)real);
     ft_putchar('.');
-    while (++byte < g_flags.accuracy)
+    while (++byte < g_flags.point)
     {
         real *= 10;
         ft_putnbr((int)real % 10);
     }
 }
-
-void ft_putstri(char *str)
-{
-    int i;
-    int len;
-
-    i = -1;
-    len = ft_strlen(str);
-    if (g_flags.accuracy)
-        len = ft_min(len, g_flags.accuracy);
-    while (++i < len)
-        ft_putchar(str[i]);
-}
-
-static void proccesing_str_flag(char *s_str)
+static void p_str_flag(char *s_str)
 {
     if (g_flags.minus)
     {
-        ft_putstri(s_str);
-        processing_width(' ', g_flags.width - ft_strlen(s_str));
+        m_putstr(s_str, 0, g_flags.accuracy);
+        m_putstr(0, ' ', g_flags.width - ft_strlen(s_str));
     }
     else
     {
-        processing_width(' ', g_flags.width - ft_strlen(s_str));
-        ft_putstri(s_str);
+		m_putstr(0, ' ', g_flags.width - ft_strlen(s_str));
+		m_putstr(s_str, 0, g_flags.accuracy);
     }
 }
 
-static int  proccesing_str(char c, va_list arg)
+static int  p_str(char c, va_list arg)
 {
     char *s_str;
 
@@ -197,11 +209,11 @@ static int  proccesing_str(char c, va_list arg)
         if (g_flags.minus)
         {
             ft_putchar(va_arg(arg, int));
-            processing_width(' ', g_flags.width - 1);
+			m_putstr(0, ' ', g_flags.width - 1);
         }
         else
         {
-            processing_width(' ', g_flags.width - 1);
+			m_putstr(0, ' ', g_flags.width - 1);
             ft_putchar(va_arg(arg, int));
         }
         return (1);
@@ -210,25 +222,25 @@ static int  proccesing_str(char c, va_list arg)
         s_str = va_arg(arg, char *);
     if (c == 'p')
         s_str = va_arg(arg, char *);
-    proccesing_str_flag(s_str);
+    processing_str_flag(s_str);
     return (1);
 }
 
-static char parse_flags_ap(va_list arg)
+static int	p_star(void)
 {
-    while (*g_format && !belongs_set(*g_format, SPECIFIIER))
-    {
-        if (*g_format == '*' || ft_isdigit(*g_format))
-            if ((g_flags.accuracy = ft_atoi(g_format)) == 0)
-                g_flags.accuracy = va_arg(arg, int);
-        g_format++;
-    }
-    return (*g_format);
+	if (*g_format == '*')
+	{
+		g_format++;
+		return va_arg(arg, int);
+	}
+	else if (ft_isdigit(*g_format))
+		return ft_atoi(g_format);
+	return (0);
 }
 
 static char parse_flags(va_list arg)
 {
-    while (*g_format && !belongs_set(*g_format, SPECIFIIER))
+    while (*g_format)
     {
         if (*g_format == '0')
             g_flags.zero = 1;
@@ -238,18 +250,16 @@ static char parse_flags(va_list arg)
             g_flags.minus = 1;
         else if (*g_format == '#')
             g_flags.oct = 1;
-        else if (*g_format == '*' || ft_isdigit(*g_format))
-        {
-            if ((g_flags.width = ft_atoi(g_format)) == 0)
-                g_flags.width = va_arg(arg, int);
-            g_format += ft_strlen(ft_lltoa(g_flags.width)) - 1;
-        }
         else if (*g_format == 'l')
-            g_flags.l = (g_flags.ll = g_flags.l) ? 0 : 1;
+			g_modifiers.l = (g_modifiers.ll = g_modifiers.l) ? 0 : 1;
         else if (*g_format == 'h')
-            g_flags.h = (g_flags.hh = g_flags.h) ? 0 : 1;
-        else if (*g_format == '.')
-            return parse_flags_ap(arg);
+			g_modifiers.h = (g_modifiers.hh = g_modifiers.h) ? 0 : 1;
+		else if ((g_flags.width = p_star()))
+			;
+        else if (*g_format == '.' && (g_flags.accurancy = p_star()))
+			;
+        else
+        	return (-1);
         g_format++;
     }
     return (*g_format);
@@ -260,46 +270,52 @@ void     parse_arg(va_list arg)
     char c;
 
     c = parse_flags(arg);
-    if (c == '%')
+    if (c == -1)
+    	return ;
+    else if (c == '%')
         ft_putchar('%');
-    else if (belongs_set(c, SPECIFIIER_INT))
+    else if (ft_strchr(SPEC_INT, c))
     {
-        proccesing_int(c, arg);
+        p_int(c, arg);
     }
-    else if (belongs_set(c, SPECIFIIER_FLOAT))
+    else if (ft_strchr(SPEC_FLOAT, c))
     {
         print_float(va_arg(arg, double));    
     }
-    else if (belongs_set(c, SPECIFIIER_STR))
+    else if (ft_strchr(SPEC_STR, c))
     {
-        proccesing_str(c, arg);
+        p_str(c, arg);
     }
 }
 
-static int search_specifier(void)
+static int	search_specifier(void)
 {
-    int i;
+	size_t	i;
 
     i = 0;
     while (g_format[i] && g_format[i] != '%')
     {
-        if (belongs_set(g_format[i], SPECIFIIER))
+        if (belongs_set(ASPEC, g_format[i]))
             return (1);
         i++;
     }
     return (0);
 }
 
-int     parse_format(va_list arg)
+static void 	update(void)
 {
-    char *pf;
+	ft_bzero(&g_flags, sizeof(g_flags));
+	ft_bzero(&g_modifiers, sizeof(g_modifiers));
+}
 
+int     		parse_format(va_list arg)
+{
     while (*g_format)
     {
         if (*g_format == '%')
         {
-            g_format++;
-            ft_bzero(&g_flags, sizeof(g_flags));
+            update();
+			g_format++;
             if (search_specifier())
 				parse_arg(arg);
             else
@@ -307,10 +323,8 @@ int     parse_format(va_list arg)
         }
         else
         {
-            pf = (char *)g_format;
             while (*g_format && *g_format != '%')
-                g_format++;
-            write(1, pf, g_format - pf);
+                ft_putchar(*g_format);
             g_format--;
         }
         g_format++;
--- src/main.c
+++ src/main.c
@@ -1,9 +1,8 @@
-#include "ft_printf.h"
+//#include "ft_printf.h"
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    ft_printf("\\");
-    printf("\\");
+	printf(argv[1], 10);
     return (0);
 }
